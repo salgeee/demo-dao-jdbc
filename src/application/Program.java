c7e08f8064039e2f12622d9844b8321f051b963d
Leonardo Salge <lferreirasalge@gmail.com>
2023-01-23 16:33:03 -0300

    implementando update

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,13 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller insert =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
+
 
 
     }
